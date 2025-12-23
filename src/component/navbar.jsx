f14c8699331390f7aeca2829b1a0c2e9f80873b5
mohamed <mrahmanibouzina07@gmail.com>
2025-12-23 12:19:53 +0000

    add navbar file

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,7 +1,53 @@
 import React from 'react'
+import { useState } from 'react';
+
+const links = [
+    { name: "Home", href: "/home" },
+    { name: "About", href: "/about" },
+    { name: "Services", href: "/services" },
+    { name: "Contact", href: "/contact" }
+];
 
 export default function NavBar() {
+
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
-    <div>navBar</div>
-  )
+    <nav className="bg-white/80 backdrop-blur-md shadow-sm fixed w-full z-50">
+        <div className='app-container flex justify-between items-center h-16'>
+            {/* logo */}
+            <span className='gradient-text text-2xl font-bold'>AI Revolotion </span>
+            {/* desktop links */}
+            <div className="hidden md:flex items-center space-x-8">
+                {links.map((link)=> ( 
+                    <a  key={link.name} 
+                        className='nav-item' 
+                        href={link.href}>
+                        {link.name} 
+                    </a>
+                ))}
+                <a href="#" className='nav-btn'>Get Started</a>
+            </div>
+            {/* mobile menu button */}
+            <button
+            onClick={()=> setMobileMenuOpen((prev)=> !prev)}
+            className='md:hidden p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+                </svg>
+            </button>
+        </div> 
+        {/* mobile menu */}
+        {mobileMenuOpen && (
+            <div className="md:hidden bg-white shadow-lg rounded-b-lg px-2 pt-2 pb-3 space-y-1">
+                {links.map((link)=> (
+                    <a className='mobile-nav-item'
+                        key={link.name}
+                        href={link.href}
+                        >{link.name}</a>
+                ))}
+                <a href="#" className='mobile-nav-btn'>Get Started</a>
+            </div>
+        ) }
+    </nav> 
+  ) 
 }
