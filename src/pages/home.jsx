cd0d59926f68466ef6eaa6e21b09b21dae41d40f
mohamed <mrahmanibouzina07@gmail.com>
2025-12-29 12:57:55 +0000

    fixed bub in home file

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import HeroSection from '../components/heroSection'
 import AiTypes from '../components/aiTypes'
 import AiBenefits from '../components/aiBenefits'
 import Contact from '../components/contact'
-import NavBar from '../components/navBar'
+import NavBar from '../components/navbar'
 import Footer from '../components/footer'
 
 export default function Home() {
